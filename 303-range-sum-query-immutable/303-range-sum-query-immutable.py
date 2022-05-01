4e1b22468161e4fce194250cc490e201c5c6985d
Shashank Shekhar <ssshekhu53@gmail.com>
2022-05-01 16:10:48 +0530

    Time: 80 ms (93.13%), Space: 17.8 MB (7.94%) - LeetHub

--- 303-range-sum-query-immutable/303-range-sum-query-immutable.py
+++ 303-range-sum-query-immutable/303-range-sum-query-immutable.py
@@ -2,17 +2,18 @@ class NumArray:
 
     def __init__(self, nums: List[int]):
         self.nums = nums
-        
+        self.cumulativeSum()
+    
+    def cumulativeSum(self):
+        self.cumulativeArr = [self.nums[0]]
+        sum = self.nums[0]
+        for i in range(1, len(self.nums)):
+            sum += self.nums[i]
+            self.cumulativeArr.append(sum)
+        print(self.cumulativeArr)
 
     def sumRange(self, left: int, right: int) -> int:
-        sum = 0
-        while left < right:
-            sum += self.nums[left] + self.nums[right]
-            left += 1
-            right -= 1
-        if left == right:
-            sum += self.nums[left]
-        return sum
+        return (self.cumulativeArr[right] - self.cumulativeArr[left-1]) if left != 0 else self.cumulativeArr[right]
         
 
 
