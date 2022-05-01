712c0b3b7e5078c1b140ecc78cc33609bf700dff
Shashank Shekhar <ssshekhu53@gmail.com>
2022-05-01 14:59:33 +0530

    Time: 76 ms (17.85%), Space: 15 MB (86.14%) - LeetHub

--- 412-fizz-buzz/412-fizz-buzz.py
+++ 412-fizz-buzz/412-fizz-buzz.py
@@ -1,6 +1,6 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
-        answer = ["" for x in range(n)]
+        answer = [x for x in range(n)]
         for i in range(1, n+1):
             if i % 3 == 0 and i % 5 == 0:
                 answer[i-1] = "FizzBuzz"
