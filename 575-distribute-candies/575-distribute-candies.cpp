bf9b8467c90ddc0b21b1bebb805cccceb2628064
Shashank Shekhar <ssshekhu53@gmail.com>
2022-05-07 12:33:21 +0530

    Time: 345 ms (54.89%), Space: 110.4 MB (69.75%) - LeetHub

--- 575-distribute-candies/575-distribute-candies.cpp
+++ 575-distribute-candies/575-distribute-candies.cpp
@@ -1,12 +1,9 @@
 class Solution {
 public:
     int distributeCandies(vector<int>& candyType) {
-        set<int> s;
+        unordered_set<int> s(candyType.begin(), candyType.end());
         int edible, count;
-        count=0;
         edible=candyType.size()/2;
-        for(int item: candyType)
-            s.insert(item);
         return min(edible, (int)s.size());
     }
 };
