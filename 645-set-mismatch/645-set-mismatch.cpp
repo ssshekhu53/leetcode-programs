c15ca25c3f5f64b19c6df0a0e1340758eab8b312
Shashank Shekhar <ssshekhu53@gmail.com>
2022-05-13 21:01:29 +0530

    Time: 43 ms (60.33%), Space: 21.4 MB (68.09%) - LeetHub

--- 645-set-mismatch/645-set-mismatch.cpp
+++ 645-set-mismatch/645-set-mismatch.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        vector<int> result;
         int x, y, n=nums.size();
         sort(nums.begin(), nums.end());
         for(int i=1; i<n; i++) {
             if(nums[i]==nums[i-1]) {
-                // result.push_back(nums[i]);
                 x=nums[i];
                 for(; i<n-1; i++) 
                     nums[i]=nums[i+1];
