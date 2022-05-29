603a71c420645fafd5cbd90c7a54fdd0984f8bc9
Shashank Shekhar <ssshekhu53@gmail.com>
2022-05-29 16:19:30 +0530

    Time: 41 ms (75.47%), Space: 13.9 MB (81.77%) - LeetHub

---
+++ 819-most-common-word/819-most-common-word.py
@@ -0,0 +1,19 @@
+class Solution:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        paragraph = paragraph.lower()
+        temp = paragraph
+        paragraph = ""
+        for ch in temp:
+            if ch.isalpha() or ch == ' ':
+                paragraph += ch
+            else:
+                paragraph += " "
+        paragraph = paragraph.split()
+        freq = {}
+        for word in paragraph:
+            if word not in banned:
+                if word not in freq:
+                    freq[word] = 0
+                freq[word] += 1
+        print(freq)
+        return max(zip(freq.values(), freq.keys()))[1]
