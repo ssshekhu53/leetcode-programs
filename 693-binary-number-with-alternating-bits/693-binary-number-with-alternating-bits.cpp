41f004d058c2ecb1813d7f388994aef6d17e5a1f
Shashank Shekhar <ssshekhu53@gmail.com>
2022-06-08 22:55:37 +0530

    Time: 3 ms (33.65%), Space: 5.7 MB (89.87%) - LeetHub

--- 693-binary-number-with-alternating-bits/693-binary-number-with-alternating-bits.cpp
+++ 693-binary-number-with-alternating-bits/693-binary-number-with-alternating-bits.cpp
@@ -4,11 +4,9 @@ public:
         long int flag, i, temp;
         i=1;
         flag=!(n&i);
-        // cout<<flag<<endl;
         i=i<<1;
         while(i<=n) {
             temp=i&n?1:0;
-            // cout<<temp<<" "<<flag<<" "<<(temp!=flag)<<endl;
             if(temp!=flag)
                 return false;
             flag=!temp;
