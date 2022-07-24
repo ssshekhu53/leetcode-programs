76c090763977ef3d811fda9ba36212dce03cc837
Shashank Shekhar <ssshekhu53@gmail.com>
2022-07-24 11:48:18 +0530

    Time: 6 ms (39.14%), Space: 6.5 MB (95.03%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
@@ -6,10 +6,10 @@ public:
         int n, count;
         n=s.length();
         count=0;
-        for(int i=0; i<n/2; i++)
+        for(int i=0; i<n/2; i++) {
             count+=Solution::isVowel(s[i])?1:0;
-        for(int i=n/2; i<n; i++)
-            count-=Solution::isVowel(s[i])?1:0;
+            count-=Solution::isVowel(s[(n/2)+i])?1:0;
+        }
         return count==0;
     }
 };
