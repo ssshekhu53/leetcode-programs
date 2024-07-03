4688496b6cd79624d13fab972ec6a0ff5f90e22c
Shashank Shekhar <ssshekhu53@gmail.com>
2024-07-03 08:24:58 +0530

    Time: 1 ms (76.48%), Space: 2.2 MB (100%) - LeetHub

---
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.go
@@ -0,0 +1,14 @@
+func numIdenticalPairs(nums []int) int {  
+    count := 0
+    n := len(nums)
+
+    for i := 0; i < n; i++ {
+        for j := i+1; j < n; j++ {
+            if nums[i] == nums[j] {
+                count++
+            }
+        }
+    }
+
+    return count
+}
