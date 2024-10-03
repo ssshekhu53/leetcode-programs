bed0f1d0df17f6a5c3236575748291f60db014e0
Shashank Shekhar <ssshekhu53@gmail.com>
2024-10-03 13:20:44 +0530

    Time: 2 ms (74.63%), Space: 3.4 MB (13.99%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.go
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.go
@@ -1,19 +1,14 @@
 func maxProfit(prices []int) int {
-    buyPrice := prices[0]
     lastPrice := prices[0]
     profit := 0
 
     for i := range prices {
-        if prices[i] < lastPrice {
-            profit += lastPrice - buyPrice
-            buyPrice = prices[i]
+        p := prices[i] - lastPrice
+        if p > 0 {
+            profit += p
         }
 
         lastPrice = prices[i]
-    }
-
-    if lastPrice > buyPrice {
-        profit += lastPrice - buyPrice
     }
 
     return profit
